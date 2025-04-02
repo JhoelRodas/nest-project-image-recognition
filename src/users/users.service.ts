0eff76e3c1476cb6e5ca4f1892e8e33bdd3c85f5
Jalter-ego <jhademels123@gmail.com>
2025-04-01 23:16:45 -0400

    rest api users

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  private users:User[] = []
+
+  create(createUserDto: CreateUserDto) {
+    this.users.push({...createUserDto,
+      id: this.users.length + 1 
+    })
+    return createUserDto;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find(user => user.id === id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const user = this.findOne(id)
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
