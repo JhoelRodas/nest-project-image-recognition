c12fd3ba93b762a045aff46181a8fe5dd1ce300e
Jalter-ego <jhademels123@gmail.com>
2025-05-15 17:18:10 -0400

    feat: add config cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,11 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ["http://localhost:5173"],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true, 
+  });
 }
 bootstrap();
