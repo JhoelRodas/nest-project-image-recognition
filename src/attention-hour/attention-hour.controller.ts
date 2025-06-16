b512c64b1d3e0f63aa07a9340178c9382e84b610
Jhoel Rodas <rodasjhoel@gmail.com>
2025-06-16 17:40:02 -0400

    feat: agregar funcionalidades para manejar múltiples usuarios en horarios de atención y mejorar la estructura del controlador y servicio

--- src/attention-hour/attention-hour.controller.ts
+++ src/attention-hour/attention-hour.controller.ts
@@ -1,31 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { AttentionHourService } from './attention-hour.service';
-import { AddAttHourUser, CreateAttentionHourDto } from './dto/create-attention-hour.dto';
+import { AddAttHourUser, AddMultipleAttHourUsers, CreateAttentionHourDto } from './dto/create-attention-hour.dto';
 import { UpdateAttentionHourDto } from './dto/update-attention-hour.dto';
 import { ApiBody, ApiParam } from '@nestjs/swagger';
 
 @Controller('attention-hour')
 export class AttentionHourController {
-  constructor(private readonly attentionHourService: AttentionHourService) {}
+  constructor(private readonly attentionHourService: AttentionHourService) { }
 
   @Post()
-  @ApiBody({type:CreateAttentionHourDto})
+  @ApiBody({ type: CreateAttentionHourDto })
   create(@Body() createAttentionHourDto: CreateAttentionHourDto) {
     return this.attentionHourService.create(createAttentionHourDto);
   }
 
   @Post('attHourUser')
-  @ApiBody({type:CreateAttentionHourDto})
+  @ApiBody({ type: CreateAttentionHourDto })
   addAttHourUser(@Body() addAttHourUser: AddAttHourUser) {
     return this.attentionHourService.addAttentionHourUser(addAttHourUser);
   }
 
   @Delete('attHourUser')
-  @ApiBody({type:CreateAttentionHourDto})
+  @ApiBody({ type: CreateAttentionHourDto })
   removeAttHourUser(@Body() addAttHourUser: AddAttHourUser) {
     return this.attentionHourService.removeAttentionHourUser(addAttHourUser);
   }
 
+  @Post('attHourUsers')
+  @ApiBody({ type: AddMultipleAttHourUsers })
+  addMultipleAttHourUsers(@Body() addMultipleAttHourUsers: AddMultipleAttHourUsers) {
+    return this.attentionHourService.addMultipleAttentionHourUsers(addMultipleAttHourUsers);
+  }
+
+  @Delete('attHourUsers')
+  @ApiBody({ type: AddMultipleAttHourUsers })
+  removeMultipleAttHourUsers(@Body() addMultipleAttHourUsers: AddMultipleAttHourUsers) {
+    return this.attentionHourService.removeMultipleAttentionHourUsers(addMultipleAttHourUsers);
+  }
 
   @Get()
   findAll() {
@@ -33,13 +44,19 @@ export class AttentionHourController {
   }
 
   @Get('organization/:id')
-  @ApiParam({name:"id",description:"id de la organizacion"})
-  findAllByOrganization(@Param('id') id:string){
+  @ApiParam({ name: "id", description: "id de la organizacion" })
+  findAllByOrganization(@Param('id') id: string) {
     return this.attentionHourService.findAllByOrganization(id)
   }
 
+  @Get('users/:id')
+  @ApiParam({ name: "id", description: "id del horario de atención" })
+  getUsersByAttentionHour(@Param('id') id: string) {
+    return this.attentionHourService.getUsersByAttentionHour(id);
+  }
+
   @Get(':id')
-  @ApiParam({name:"id",description:"id del horario de atencion"})
+  @ApiParam({ name: "id", description: "id del horario de atencion" })
   findOne(@Param('id') id: string) {
     return this.attentionHourService.findOne(id);
   }
@@ -50,7 +67,7 @@ export class AttentionHourController {
   }
 
   @Delete(':id')
-  @ApiParam({name:"id",description:"id del horario de atencion"})
+  @ApiParam({ name: "id", description: "id del horario de atencion" })
   remove(@Param('id') id: string) {
     return this.attentionHourService.remove(id);
   }
